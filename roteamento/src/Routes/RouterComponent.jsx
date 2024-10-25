e9590f96d9ae7632b9025ab6c59738a217121c25
Nathan Vieira <nathanrodriguesvieira.contato@gmail.com>
2024-10-25 07:37:29 -0300

    pasta routes com componentes de roteamento

---
+++ roteamento/src/Routes/RouterComponent.jsx
@@ -0,0 +1,16 @@
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+
+])
+
+function RouterComponent() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default RouterComponent
